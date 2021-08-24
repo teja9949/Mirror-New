b49d14b21f15b250c68cfef849a4f837d527d5a1
yuno74 <65702879+yuno74@users.noreply.github.com>
2021-08-24 15:25:59 +0800

    Update reboot.py

--- bot/modules/reboot.py
+++ bot/modules/reboot.py
@@ -2,10 +2,10 @@
 # Based on this https://github.com/DevsExpo/FridayUserbot/blob/master/plugins/heroku_helpers.py
 
 from pyrogram import filters
-from bot import app, OWNER_ID, bot
+from bot import app, OWNER_ID, bot, REBOOT_BOT
 from bot.helper import check_heroku
 
-@app.on_message(filters.command(['reboot', f'reboot@{bot.username}']) & filters.user(OWNER_ID))
+@app.on_message(filters.command([f'{REBOOT_BOT}', f'{REBOOT_BOT}t@{bot.username}']) & filters.user(OWNER_ID))
 @check_heroku
 async def gib_restart(client, message, hap):
     msg_ = await message.reply_text("[HEROKU] - Restarting")
